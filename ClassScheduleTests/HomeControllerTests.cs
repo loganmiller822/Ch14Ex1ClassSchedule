c4d9492a82b04e3c40263a1180127186a6852480
Logan Miller <logan.miller@southeasttech.edu>
2024-04-14 15:38:45 -0500

    Write another test and run it.

---
+++ ClassScheduleTests/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using ClassSchedule.Controllers;
+using ClassSchedule.Models;
+
+namespace ClassScheduleTests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionMethod_ReturnsAViewResult()
+        {
+            var mockUnitOfWork = new Mock<IClassScheduleUnitOfWork>();
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var controller = new HomeController(mockUnitOfWork.Object, mockHttpContextAccessor.Object);
+
+            var result = controller.Index(0);
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
--- ClassScheduleTests/TeacherControllerTests.cs
+++ ClassScheduleTests/TeacherControllerTests.cs
@@ -4,7 +4,6 @@ using Moq;
 using Microsoft.AspNetCore.Mvc;
 using ClassSchedule.Models;
 using ClassSchedule.Controllers;
-using Microsoft.AspNetCore.Http;
 
 public class TeacherControllerTests
 {
